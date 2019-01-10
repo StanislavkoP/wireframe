ce766d9edaa65516808eca3fd76d60afb746aed7
StanislavkoP <stas8327634@gmail.com>
2019-01-10 21:41:41 +0200

    Added props validation and render phone item components

--- src/components/PhoneList/PhoneList.js
+++ src/components/PhoneList/PhoneList.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-import PhoneItem from './PhoneItem';
+import './PhoneList.css';
+
+import PhoneItem from './PhoneItem/PhoneItem';
+
+PhoneList.defaultProps = {
+	phoneList: [],
+};
 
 PhoneList.propTypes = {
-
-}
+	phoneList: PropTypes.array,
+};
 
 function PhoneList(props) {
+	const { phoneList } = props;
+
 	return (
-		<div className="ui celled list">
-			
+		<div className="ui link items phoneList">
+			{
+				phoneList.map( (phone, index) => (
+					<PhoneItem
+						key={ index }
+						avatar={ phone.general.avatar }
+						firstName={ phone.general.firstName }
+						lastName={ phone.general.lastName }
+						jobTitle={ phone.job.title }
+					/>
+				))
+			}
 		</div>
-	)
-}
+	);
+};
 
 export default PhoneList;
