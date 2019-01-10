8211bd4b702551427ad6053f2d27cee21c03c62b
StanislavkoP <stas8327634@gmail.com>
2019-01-10 20:11:11 +0200

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './App';
+
+import reducer from './state/reducers/phone';
+
+const rootReducer = reducer;
+
+
+const configurateStore = (initialState = {}) => {
+	const middleWares = [
+		thunk
+	]
+
+	const enchancers = [
+		applyMiddleware(...middleWares)
+	]
+
+	const store = createStore(
+		rootReducer,
+		initialState,
+		composeWithDevTools(...enchancers)
+	)
+
+	return store
+}
+
+const store = configurateStore({});
+
+
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+
+	, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
