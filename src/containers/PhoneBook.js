257258a21a4188ce3bd1e98944e8804247093470
StanislavkoP <stas8327634@gmail.com>
2019-01-10 20:12:36 +0200

    Connected component to Redux

--- src/containers/PhoneBook.js
+++ src/containers/PhoneBook.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+import * as actions from '../state/actions/index';
 
 import PhoneList from '../components/PhoneList/PhoneList';
 
 class PhoneBook extends Component {
-	
+
 	componentDidMount() {
-		
+		this.props.getPhoneList();
 	}
 
 	render() {
@@ -24,5 +28,12 @@ class PhoneBook extends Component {
 	}
 };
 
+const mapStateToProps = state => ({
+	phoneList: state.phoneList
+});
 
-export default PhoneBook;
+const mapDispatchToProps = dispatch => ({
+	getPhoneList : () => dispatch( actions.getPhoneList() )
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneBook);
