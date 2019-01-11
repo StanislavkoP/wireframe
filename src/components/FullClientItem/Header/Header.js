457e67b51c6b6e411776fc71d872b64a08bbf78e
StanislavkoP <stas8327634@gmail.com>
2019-01-11 23:29:31 +0200

    Broked  into components and added placeholder for avatar

---
+++ src/components/FullClientItem/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { isEmpty } from '../../../utils/validation';
+
+FullClientItemContacts.defaultProps = {
+	general: {},
+	job: {},
+};
+
+FullClientItemContacts.propTypes = {
+	general: PropTypes.object.isRequired,
+	job: PropTypes.object.isRequired,
+};
+
+function FullClientItemContacts(props) {
+	const { general, job, } = props;
+
+	const firstName = !isEmpty(general.firstName) ? general.firstName : 'Name';
+	const lastName = !isEmpty(general.lastName) ? general.lastName : '';
+	const jobTitle = !isEmpty(job.title) ? job.title : '';
+	const jobCompany = !isEmpty(job.company) ? job.company : '';
+
+
+	return (
+		<div className="fullPhoneItem__header">
+			<h1>{ `${ firstName } ${ lastName }` }</h1>
+			<h4>{ `${ jobTitle } — ${ jobCompany }` }</h4>
+		</div>
+	);
+};
+
+
+
+export default FullClientItemContacts;
+
