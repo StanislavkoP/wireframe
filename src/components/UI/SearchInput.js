cbdb044f796cc71cf1725e716a4f4f205e803e98
StanislavkoP <stas8327634@gmail.com>
2019-01-11 15:27:11 +0200

    Added props

--- src/components/UI/SearchInput.js
+++ src/components/UI/SearchInput.js
@@ -2,14 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 SearchInput.propTypes = {
-
+	value: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
 }
 
 function SearchInput(props) {
+	const { value, onChange } = props;
+
 	return (
-		<div class="ui icon input">
-			<input type="text" placeholder="Search..." />
-			<i class="search icon"></i>
+		<div className="ui fluid icon input">
+			<input type="text" value={value} onChange={onChange} placeholder="Search..." />
+			<i className="search icon"></i>
 		</div>
 	)
 };
