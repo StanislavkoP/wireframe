752242b5acb1b9f3456d80c19432625fb9b33dda
StanislavkoP <stas8327634@gmail.com>
2019-01-13 21:34:48 +0200

    Renamed file to Client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-import ClientList from './containers/ClientList';
+import Client from './containers/Client';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="appWrap" style={{overflowX: 'hidden', height: 'calca'}}>
-				<ClientList />
+				<Client />
 			</div>
 		);
 	}
